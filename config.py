08402a035f164982bb4d13acacac8e3e773b0018
Anaximandr228 <147671916+Anaximandr228@users.noreply.github.com>
2024-07-15 04:17:50 +0500

    Update config.py
    Delete my config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 host = '127.0.0.1' #Хост приложения
-user = "postgres" #Имя пользователя PostgresSQL
-password = "rtf558Ur" #Пароль пользователя PostgresSQL
-db_name = "Task" #Название базы данных PostgesSQL
+user = "" #Имя пользователя PostgresSQL
+password = "" #Пароль пользователя PostgresSQL
+db_name = "" #Название базы данных PostgesSQL
