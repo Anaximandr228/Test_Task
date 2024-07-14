43c587184f265acddf04459ed08e3c51e60fc6f6
Anaximandr228 <147671916+Anaximandr228@users.noreply.github.com>
2024-07-15 04:20:20 +0500

    Add comment

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ from models import Users, db
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rtf558Ur@localhost/Task'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/db_name' #Смените 'user' 'password' 'db_name' на свои данные PostgresSQL
 db.init_app(app)
 
 if __name__ == '__main__':
